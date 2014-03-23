03f2b8581f8eaba6c112f0c581aed939fb53a0e4
John Sigvald Skauge <thejohnskauge@gmail.com>
2014-03-23 21:41:09 +0100

    First real commit

---
+++ main.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+import json
+
+__author__ = 'Modulus'
+import urllib2
+
+
+def dateformat():
+    return "%d.%m.%Y"
+
+
+def run():
+    start_date = datetime.date(datetime(1986, 01, 01))
+
+    current_date = datetime.now()
+
+    lists = generate_lists(start_date.strftime(dateformat()), current_date.strftime(dateformat()))
+    numbers = {}
+    for index, value in enumerate(lists[1]):
+        numbers[index+1] = value
+
+    print numbers
+
+
+def generate_lists(start_date, end_date):
+    base_url = "https://www.norsk-tipping.no/miscellaneous/getNumberStatisticsVikingLotto.htm?fromDate={0}&toDate={1}&".format(start_date, end_date)
+
+    url = urllib2.urlopen(base_url)
+
+    content = url.readlines()
+
+    """
+    The first array has the actual numbers,
+    The second array is amount of times this number has been selected
+    The third array is how many times this number has been additional numbers
+    """
+
+    for line in content:
+        if "var sta_dataTable" in line:
+            json_array = line.split("=")[1]
+            json_array = json_array.replace(";", "")
+            return json.loads(json_array)
+
+
+if __name__ == "__main__":
+    run()
