1632ec87faf19b8a4c6c4fc349c40c20830a17d2
Modulus <thejohnskauge@gmail.com>
2014-03-24 21:01:20 +0100

    Added first tests and dateutils

---
+++ tests/date_utils_test.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from unittest import TestCase
+from dateutil.relativedelta import relativedelta
+from utilities.date_utils import DateUtils
+
+__author__ = 'Modulus'
+
+
+class MainTests(TestCase):
+
+    def setUp(self):
+        self.start = datetime.date(datetime(1986, 01, 01))
+        self.end = datetime.date(datetime(2014, 03, 24))
+        self.util = DateUtils()
+
+    def test_timedelta(self):
+        dates = self.util.get_dates(self.start)
+        self.assertIsNotNone(dates)
+        self.assertNotEquals([], dates)
+        self.assertEquals(9, len(dates))
+
+        self.assertEquals(1986, dates[0].year)
+        self.assertEquals(1996, dates[1].year)
+        self.assertEquals(2006, dates[2].year)
+        self.assertEquals(1992, dates[3].year)
+        self.assertEquals(1998, dates[4].year)
+        self.assertEquals(2004, dates[5].year)
+        self.assertEquals(2010, dates[6].year)
+        self.assertEquals(2016, dates[7].year)
+        self.assertEquals(2022, dates[8].year)
+
---
+++ utilities/date_utils.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from math import ceil
+from dateutil.relativedelta import relativedelta
+
+__author__ = 'Modulus'
+
+
+class DateUtils(object):
+
+    def get_dates(self, start_date, end_date=datetime.now()):
+        dates = []
+        if start_date == datetime.today():
+            dates.append(datetime.now())
+        elif start_date and end_date:
+            delta1 = relativedelta(end_date, start_date).years
+            decade = int(ceil(delta1 / 10.0))
+
+            current_date = start_date
+            for value in range(0, decade):
+                dates.append(datetime.date(datetime(current_date.year, 12, 31)))
+                current_date += relativedelta(years=10)
+
+            rest = 9 - len(dates)
+            current_date = start_date
+            new_dates = []
+            for value in range(0, decade):
+                for sub_value in range(0, rest / len(dates)):
+                    current_date += relativedelta(years=rest)
+                    new_dates.append(current_date)
+
+            dates.extend(new_dates)
+
+        return dates
